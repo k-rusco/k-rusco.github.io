91d733c0e06bedfbb2384d540211dff6fecc7485
k-rusco <krusconi@zhdk.ch>
2025-11-26 10:06:53 +0100

    1st

--- projects/01MainAssignments/project1/script.js
+++ projects/01MainAssignments/project1/script.js
@@ -1,20 +1,94 @@
-let len = 10;
-let bg;
+let titlefont;
+let subfont;
+let movingText = 'ui/ux designer ~ illustrator ~ graphic designer ~ animator ~ motion designer ~ creative coder ~ videomaker & photographer ~ ui/ux designer ~ illustrator ~ graphic designer ~ animator ~ motion designer ~ creative coder ~ videomaker & photographer ~ ui/ux designer ~ illustrator ~ graphic designer ~ animator ~ motion designer ~ creative coder ~ videomaker & photographer ~ '
+let clicked= true;
+let bg=255;
+
+
+function preload(){
+	titlefont = loadFont(fonts/PLANK.otf)
+	subfont = loadFont(fonts/Karina.otf)
+}
 
 function setup() {
+	
 	createCanvas(windowWidth,windowHeight,WEBGL);
-	background(0);
-	frameRate(30);
 	rectMode(CENTER);
-	bg = color(0);
+	
+	frameRate(30)
+	noStroke();
 }
 
 function  draw() {
-	background(bg);
-	fill(255)
-    textSize(width / 3);
-	rotateY(mouseX)
-	text('Kiiwmee', 100, 100)
+	background(bg)
+
+	push()
+	translate(-1000,-170,-100)
+	sub()
+	pop()
+
+	push()
+	translate(-4000,100,-100)
+	rotateX(500)
+	sub2()
+	pop()
+
+	
+	
+	push()
+	translate(-20,-100,-100);
+	fill(0,255,0)
+	textFont(titlefont);
+   textSize(120);
+	textAlign(CENTER)
+	rotateY(mouseX*0.012)
+	text('Kiiwmee',90, 100)
+	pop()
+
+	push()
+	translate(0, 0, 490)
+	 if (clicked) {
+     blendMode(BLEND);  
+     fill(255, 255, 255, 0)   
+     circle(-150, -120, 200);
+   } else {
+     blendMode(EXCLUSION); 
+     fill(255)      
+     circle(-150, -120, 2000);
+   }
+	pop()
+
+	mousecursor()
 	
 }
 
+
+function mousecursor(){
+	push()
+	translate(-width/2 + mouseX, -height/2 + mouseY, 500)
+	fill(255)
+	blendMode(EXCLUSION)
+	circle(-width/2 + mouseX, -height/2 + mouseY, 75)
+	pop()
+}
+
+function sub(){	
+	fill(0)
+	textSize(28)
+	textFont(subfont)
+	text(movingText, frameCount*-2, -1)
+}
+
+function sub2(){	
+	fill(0)
+	textSize(28)
+	textFont(subfont)
+	text(movingText, frameCount*2, -1)
+}
+
+
+function mouseClicked(){
+	
+	clicked = !clicked
+
+}
